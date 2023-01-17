054599351fa0de331b032e3065231999dcb90517
PlainsVillager <littlecuckoo@outlook.com>
2023-01-17 11:05:58 +0800

    api in

---
+++ StringEqualsAPI/src/github/plainsvillager/stringeaualsapi/StringEUtil.java
@@ -0,0 +1,13 @@
+package github.plainsvillager.stringeaualsapi;
+
+public class StringEUtil {
+
+    private StringEUtil(){
+
+    }
+
+    public static boolean judge(String cmp1, String cmp2){
+
+        return false;
+    }
+}
